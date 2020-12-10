1abdaca07586410b92040226f2b97510984fbaea
Armin <40383183+Preefix@users.noreply.github.com>
2020-12-10 16:29:11 +0100

    added landing

---
+++ resources/js/components/modals/JoinLobbyModal.vue
@@ -0,0 +1,47 @@
+<template>
+    <div class="modal fade" id="joinLobbyModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">Lobby beitreten</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        <input v-model="joinCode" class="form-control" type="text" placeholder="Gib hier deinen Join Code ein">
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Abbrechen</button>
+        <button type="button" class="btn btn-primary" @click="joinLobby">Beitreten</button>
+      </div>
+    </div>
+  </div>
+</div>
+</template>
+
+<script>
+import * as authService from '../../services/auth_service';
+export default {
+    data() {
+        return {
+            joinCode: ''
+        }
+    },
+    methods: {
+        joinLobby() {
+            console.log("lobby joined");
+            console.log(this.joinCode);
+            this.$router.push({name: 'conceptPaper', params: { joincode: this.joinCode }});
+            this.flashMessage.error({
+                    message: 'Ups, es scheint, dass der Join Code nicht korrekt ist. Probier es noch einmal.',
+                    time: 5000
+                });
+        }
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
--- resources/js/views/Hello.vue
+++ resources/js/views/Hello.vue
@@ -1,57 +1,124 @@
  <template>
-  <div class="flex-center position-ref full-height">
-    <div class="content">
-      <div class="m-b-md">
-        <h2 class="title m-b-md">conForm</h2>
-        <h3>Your efficent task planner</h3>
+ <div class="hero">
+    <div class="container">
+      <div class="row">
+        <div class="col col--11 col-offset-1">
+          <div class="headline-hero">
+             <h1 class="hero--title">Colloboration war noch nie so einfach.</h1>
+              <div class="hero--lead">
+               conForm entfesselt dein ganzes Potential.
+            </div>
+          </div>
+          <a href="" class="button hero--button">Wie kann ich loslegen?</a>
+        </div>
+        <div class="col">
+          <img src="../../assets/join-lobby.svg" />
+      </div>
       </div>
     </div>
-  </div>
+ </div>
 </template>
 
-  <style scoped>
-.full-height {
-  height: 100vh;
+<style scoped>
+
+.container, .row .col {
+    padding: 0 1rem;
+    width: 100%;
 }
-.flex-center {
-  align-items: center;
+
+.row .col {
+    max-width: 100%;
+    flex: 1 0;
+    margin-left: 0;
+}
+
+.row .col, img {
+    max-width: 100%;
+}
+
+.hero--illustration {
+    display: flex;
+    height: 637px;
+    top: -57px;
+    right: 35px;
+}
+.hero--button, .hero--lead, .hero--title {
+    transform: translateY(29px) translateX(22px);
+}
+.button {
+    display: inline-block;
+    font-family: "Poppins",sans-serif !important;
+    font-size: 27px;
+    color: #fff;
+    letter-spacing: 0;
+    text-align: center;
+    text-decoration: none;
+    background: #ff7e85;
+    border-radius: 3px;
+    padding: 18px 48px 14px;
+    font-weight: 500;
+}
+.hero {
+    position: relative;
+    background-color: #171c5d;
+    height: 785px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    margin: 0 0 99px;
+}
+
+.hero--lead {
+  font-size: 36px;
+    line-height: 33px;
+    color: #6c72ae;
+    letter-spacing: 0;
+    max-width: 568px;
+    margin: 0 0 33px;
+}
+
+.hero--title {
+  font-family: "Poppins",sans-serif !important;
+   font-size: 47px;
+    line-height: 55px;
+    color: #fff;
+    letter-spacing: 0;
+    margin: 0;
+    max-width: 674px;
+    font-weight: 400;
+    margin-bottom: 15px;
+}
+
+.headline-hero {
+    margin-bottom: 2rem;
+}
+
+.container, .row .col{
+    padding: 0 1rem;
+    width: 100%;
+}
+.row .col.col--11 {
+    flex: 0 0 50%;
+    max-width: 50%;
+}
+.row {
   display: flex;
-  justify-content: center;
+  flex-direction: row;
+  flex-wrap: row;
+  margin: 0 calc(1rem * -1);
 }
-.position-ref {
-  position: relative;
-}
-.top-right {
-  position: absolute;
-  right: 10px;
-  top: 18px;
-}
-.content {
-  text-align: center;
-}
-.title {
-  font-size: 60px;
-}
-.links > a {
-  color: #636b6f;
-  padding: 0 25px;
-  font-size: 12px;
-  font-weight: 600;
-  letter-spacing: 0.1rem;
-  text-decoration: none;
-  text-transform: uppercase;
-}
-.m-b-md {
-  margin-bottom: 30px;
-  color: #000000;
+.container {
+  max-width: 1140px;
+  margin: 0 auto;
 }
 
 </style>
 
 <script>
-    export default {
-      components: {
-       
-      }
-    }
+import JoinLobbyModal from "../components/modals/JoinLobbyModal.vue";
+export default {
+  components: {
+    JoinLobbyModal,
+  },
+};
 </script>
