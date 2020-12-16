4611199cfa4f78ff935ca467665f14ffb047d0fc
Armin <40383183+Preefix@users.noreply.github.com>
2020-12-16 15:13:35 +0100

    changed navbar layout

--- resources/js/components/NavBar.vue
+++ resources/js/components/NavBar.vue
@@ -1,87 +1,110 @@
 <template>
   <nav class="navbar sticky-top navbar-expand-md navbar-light navbar-laravel">
-      <div class="container">
-        <router-link :to="{ name: 'welcome' }" class="navbar-brand">
-          <img
-            src="../../assets/conForm_logo.png"
-            width="80"
-            height="80"
-            alt=""
-          />
-        </router-link>
-        <button
-          class="navbar-toggler"
-          type="button"
-          data-toggle="collapse"
-          data-target="#navbarSupportedContent"
-          aria-controls="navbarSupportedContent"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span class="navbar-toggler-icon"></span>
-        </button>
+    <div class="container">
+      <router-link :to="{ name: 'welcome' }" class="navbar-brand">
+        <img
+          src="../../assets/conForm_logo.png"
+          width="80"
+          height="80"
+          alt=""
+        />
+      </router-link>
+      <button
+        class="navbar-toggler"
+        type="button"
+        data-toggle="collapse"
+        data-target="#navbarSupportedContent"
+        aria-controls="navbarSupportedContent"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span class="navbar-toggler-icon"></span>
+      </button>
 
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-          <!-- Left Side Of Navbar -->
-          <ul class="navbar-nav mr-auto"></ul>
-           <ul class="navbar-nav mr-auto"></ul>
-          <!-- Right Side Of Navbar -->
-          <ul class="navbar-nav ml-auto">
-        
-            <router-link
-              :to="{ name: 'pusher' }"
-              class="nav-link"
-              v-if="isLoggedIn"
-              >Pusher</router-link
-            >
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <!-- Left Side Of Navbar -->
+        <ul class="navbar-nav mr-auto"></ul>
+        <ul class="navbar-nav mr-auto"></ul>
+        <!-- Right Side Of Navbar -->
+        <ul class="navbar-nav ml-auto">
 
-            <router-link
-              :to="{ name: 'conceptPaperCreator' }"
-              class="nav-link"
-              v-if="isLoggedIn"
-              >Concept Paper Creator</router-link
+           <!-- Dark Mode toggle -->
+          <Toggle :mode="mode" @toggle="$emit('toggle')" />
+          
+          <!-- Dropdown -->
+          <li class="nav-item dropdown">
+            <a
+              class="nav-link dropdown-toggle"
+              href="#"
+              id="navbarDropdown"
+              role="button"
+              data-toggle="dropdown"
+              aria-haspopup="true"
+              aria-expanded="false"
             >
+              Produkte
+            </a>
+            <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+              <a class="dropdown-item">
+                <router-link
+                  :to="{ name: 'pusher' }"
+                  class="nav-link"
+                  v-if="isLoggedIn"
+                  >Pusher</router-link
+                >
+              </a>
+              <a class="dropdown-item">
+                <router-link
+                  :to="{ name: 'conceptPaperCreator' }"
+                  class="nav-link"
+                  v-if="isLoggedIn"
+                  >Konzeptpapier Creator</router-link
+                ></a
+              >
+              <div class="dropdown-divider"></div>
+              <a class="dropdown-item" href="#">
+                <router-link
+                  :to="{ name: 'kanban' }"
+                  class="nav-link"
+                  v-if="isLoggedIn"
+                  >Kanban Board</router-link
+                ></a
+              >
+            </div>
+          </li>
 
-             <router-link
-              :to="{ name: 'kanban' }"
-              class="nav-link"
-              v-if="isLoggedIn"
-              >Task planner</router-link
-            >
 
-            <!-- Dark Mode toggle -->
-            <Toggle :mode="mode" @toggle="$emit('toggle')" />
-            <!-- Authentication Links -->
-            <router-link
-              :to="{ name: 'login' }"
-              class="nav-link nav-link-login"
-              v-if="!isLoggedIn"
-              >Login</router-link
-            >
-            <router-link
-              :to="{ name: 'register' }"
-              class="nav-link nav-link-register"
-              v-if="!isLoggedIn"
-              >Register</router-link
-            >
-            <li class="nav-link" v-if="isLoggedIn">Hi, {{ name }}</li>
+          <!-- Authentication Links -->
+          <router-link
+            :to="{ name: 'login' }"
+            class="nav-link nav-link-login"
+            v-if="!isLoggedIn"
+            >Login</router-link
+          >
+          <router-link
+            :to="{ name: 'register' }"
+            class="nav-link nav-link-register"
+            v-if="!isLoggedIn"
+            >Register</router-link
+          >
+          <li class="nav-link" v-if="isLoggedIn">Hi, {{ name }}</li>
 
-            <button v-if="isLoggedIn" class="btn btn-logout" @click="logout">
-              Logout
-            </button>
-          </ul>
-        </div>
+          <button v-if="isLoggedIn" class="btn btn-logout" @click="logout">
+            Logout
+          </button>
+        </ul>
       </div>
+    </div>
   </nav>
 </template>
 
 <script>
 import * as auth from "../services/auth_service";
-import Toggle from '../components/Toggler.vue';
+import Toggle from "../components/Toggler.vue";
 export default {
-  props: ['mode'],
+  props: ["mode"],
   components: {
-    Toggle
+    Toggle,
   },
   data() {
     return {
@@ -127,10 +150,10 @@ export default {
   margin-left: auto;
   margin-right: auto;
   position: fixed;
-} 
+}
 
 .navbar-collapse a {
-text-align: center;
+  text-align: center;
 }
 
 .nav-link {
@@ -147,13 +170,13 @@ text-align: center;
 
 .btn-logout {
   background-color: #5c55ba;
-color: #fff;
+  color: #fff;
 }
 
 @media (min-width: 768px) {
-.navbar-expand-md .navbar-nav .nav-link {
+  .navbar-expand-md .navbar-nav .nav-link {
     padding-right: 1rem;
     padding-left: 1rem;
-}
+  }
 }
 </style>
