45422888961d14204f84974bd9ef8767c0cb13af
Armin <40383183+Preefix@users.noreply.github.com>
2021-01-09 16:16:58 +0100

    proxies

--- app/Http/Middleware/TrustProxies.php
+++ app/Http/Middleware/TrustProxies.php
@@ -19,5 +19,7 @@ class TrustProxies extends Middleware
      *
      * @var int
      */
-    protected $headers = Request::HEADER_X_FORWARDED_ALL;
+    //protected $headers = Request::HEADER_X_FORWARDED_ALL;
+
+    protected $headers = Request::HEADER_X_FORWARDED_AWS_ELB;
 }
